ab3762f32fac6b15e3304da26edad5b571a9e695
Milan Aryal <considermilan@gmail.com>
2021-04-13 07:22:17 +0545

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # imagemin
 Use Imagemin to compress images
+
+[Readme](https://web.dev/use-imagemin-to-compress-images/).
---
+++ index.js
@@ -0,0 +1,16 @@
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+
+;(async () => {
+  const files = await imagemin(['./input/*.{jpg,png}'], {
+    destination: './output/',
+    plugins: [
+      imageminMozjpeg({ quality: 40 }),
+      imageminPngquant({ quality: [0.6, 0.8] }),
+    ],
+  })
+
+  console.log(files)
+  //=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
+})()
